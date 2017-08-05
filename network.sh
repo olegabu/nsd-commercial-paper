27f2da8b88aa1e675229156ff7df9f81f5677504
Oleg Abdrashitov <olegabu73@gmail.com>
2017-08-05 17:31:34 -0400

    working with 3 member channel, 2 api servers; copied from fabric-rest

--- network.sh
+++ network.sh
@@ -7,6 +7,10 @@ ORG3=a
 ORG4=b
 CHANNEL_NAME="$ORG2-$ORG3"
 
+CHAINCODE_NAME=mycc
+CHAINCODE_PATH=chaincode_example02
+CHAINCODE_INIT='{"Args":["init","a","100","b","200"]}'
+
 CLI_TIMEOUT=10000
 COMPOSE_FILE=ledger/docker-compose.yaml
 COMPOSE_TEMPLATE=ledger/docker-compose-template.yaml
@@ -31,18 +35,29 @@ function removeArtifacts() {
   rm -rf artifacts/channel
 }
 
-function removeDockers() {
-  if [ -f ${COMPOSE_FILE} ] && [ -s ${COMPOSE_FILE} ]; then
-    echo "Removing docker containers"
+function removeDockersFromCompose() {
+  if [ -f ${COMPOSE_FILE} ]; then
+    echo "Removing docker containers listed in $COMPOSE_FILE"
     docker-compose -f ${COMPOSE_FILE} kill
     docker-compose -f ${COMPOSE_FILE} rm -f
   else
-    echo "No generated docker-compose.yaml and no docker instances to remove"
+    echo "No generated $COMPOSE_FILE and no docker instances to remove"
   fi;
 }
 
+function removeDockersWithDomain() {
+  SEARCH="$DOMAIN"
+  DOCKER_IDS=$(docker ps -a | grep ${SEARCH} | awk '{print $1}')
+  if [ -z "$DOCKER_IDS" -o "$DOCKER_IDS" == " " ]; then
+    echo "No docker instances available for deletion with $SEARCH"
+  else
+    echo "Removing docker instances found with $SEARCH: $DOCKER_IDS"
+    docker rm -f ${DOCKER_IDS}
+  fi
+}
+
 function generateArtifacts() {
-    echo "Creating yaml files with names $DOMAIN, $ORG1, $ORG2, $ORG3, $ORG4"
+    echo "Creating yaml files with $DOMAIN, $ORG1, $ORG2, $ORG3, $ORG4"
     # configtx and cryptogen
     sed -e "s/DOMAIN/$DOMAIN/g" -e "s/DOMAIN/$DOMAIN/g" -e "s/ORG1/$ORG1/g" -e "s/ORG2/$ORG2/g" -e "s/ORG3/$ORG3/g" -e "s/ORG4/$ORG4/g" -e "s/CHANNEL_NAME/$CHANNEL_NAME/g" artifacts/configtxtemplate.yaml > artifacts/configtx.yaml
     sed -e "s/DOMAIN/$DOMAIN/g" artifacts/cryptogentemplate-orderer.yaml > artifacts/"cryptogen-$DOMAIN.yaml"
@@ -53,7 +68,14 @@ function generateArtifacts() {
     # docker-compose.yaml
     sed -e "s/DOMAIN/$DOMAIN/g" -e "s/ORG1/$ORG1/g" -e "s/ORG2/$ORG2/g" -e "s/ORG3/$ORG3/g" -e "s/ORG4/$ORG4/g" -e "s/CHANNEL_NAME/$CHANNEL_NAME/g" $COMPOSE_TEMPLATE > ${COMPOSE_FILE}
     # network-config.json
-    sed -e "s/DOMAIN/$DOMAIN/g" -e "s/ORG1/$ORG1/g" -e "s/ORG2/$ORG2/g" -e "s/ORG3/$ORG3/g" -e "s/ORG4/$ORG4/g" -e "s/CHANNEL_NAME/$CHANNEL_NAME/g" artifacts/network-config-template.json > artifacts/network-config.json
+    #  fill environments                                                   |   remove comments
+    sed -e "s/\DOMAIN/$DOMAIN/g" -e "s/\ORG1/$ORG1/g" -e "s/\ORG2/$ORG2/g" -e "s/^\s*\/\/.*$//g" artifacts/network-config-template.json > artifacts/network-config.json
+#    sed -e "s/\DOMAIN/$DOMAIN/g" -e "s/\ORG1/$ORG1/g" -e "s/\ORG2/$ORG2/g" -e "s/^\s*\/\/.*$//g" artifacts-dev/network-config-template.json > artifacts-dev/network-config.json
+    # fabric-ca-server-config.yaml
+    sed -e "s/ORG/$ORG1/g" artifacts/fabric-ca-server-configtemplate.yaml > artifacts/"fabric-ca-server-config-$ORG1.yaml"
+    sed -e "s/ORG/$ORG2/g" artifacts/fabric-ca-server-configtemplate.yaml > artifacts/"fabric-ca-server-config-$ORG2.yaml"
+    sed -e "s/ORG/$ORG3/g" artifacts/fabric-ca-server-configtemplate.yaml > artifacts/"fabric-ca-server-config-$ORG3.yaml"
+    sed -e "s/ORG/$ORG4/g" artifacts/fabric-ca-server-configtemplate.yaml > artifacts/"fabric-ca-server-config-$ORG4.yaml"
 
     echo "Generating crypto material with cryptogen"
     docker-compose --file ${COMPOSE_FILE} run --rm "cli.$DOMAIN" bash -c "cryptogen generate --config=cryptogen-$DOMAIN.yaml"
@@ -82,32 +104,88 @@ function generateArtifacts() {
     chown -R 1000:1000 artifacts/
 }
 
+function createChannel () {
+    info "creating channel $CHANNEL_NAME by $ORG1"
+
+    docker-compose --file ${COMPOSE_FILE} run --rm "cli.$ORG1.$DOMAIN" bash -c "peer channel create -o orderer.$DOMAIN:7050 -c $CHANNEL_NAME -f /etc/hyperledger/artifacts/channel/$CHANNEL_NAME.tx --tls --cafile /etc/hyperledger/crypto/orderer/tls/ca.crt"
+}
+
+function joinChannel() {
+    ORG=$1
+    info "joining channel $CHANNEL_NAME by $ORG"
+
+    docker-compose --file ${COMPOSE_FILE} run --rm "cli.$ORG.$DOMAIN" bash -c "CORE_PEER_ADDRESS=peer0.$ORG.$DOMAIN:7051 peer channel join -b $CHANNEL_NAME.block      && CORE_PEER_ADDRESS=peer1.$ORG.$DOMAIN:7051 peer channel join -b $CHANNEL_NAME.block"
+}
+
+function instantiateChaincode () {
+    ORG=$1
+    N=$2
+    I=$3
+    info "instantiating chaincode $N on $CHANNEL_NAME by $ORG with $I"
+
+    docker-compose --file ${COMPOSE_FILE} run --rm "cli.$ORG.$DOMAIN" bash -c "CORE_PEER_ADDRESS=peer0.$ORG.$DOMAIN:7051 peer chaincode instantiate -n $N -v 1.0 -c '$I' -o orderer.$DOMAIN:7050 -C $CHANNEL_NAME --tls --cafile /etc/hyperledger/crypto/orderer/tls/ca.crt"
+}
+
+function installChaincode() {
+    ORG=$1
+    N=$2
+    P=$3
+    info "installing chaincode $N to peers of $ORG from $P"
+
+    docker-compose --file ${COMPOSE_FILE} run --rm "cli.$ORG.$DOMAIN" bash -c "CORE_PEER_ADDRESS=peer0.$ORG.$DOMAIN:7051 peer chaincode install -n $N -v 1.0 -p $P      && CORE_PEER_ADDRESS=peer1.$ORG.$DOMAIN:7051 peer chaincode install -n $N -v 1.0 -p $P"
+}
+
 function networkUp () {
   # generate artifacts if they don't exist
-#  if [ ! -d "artifacts/crypto-config" ]; then
-#    generateArtifacts
-#  fi
-  TIMEOUT=$CLI_TIMEOUT docker-compose -f ${COMPOSE_FILE} up -d 2>&1
+  if [ ! -d "artifacts/crypto-config" ]; then
+    generateArtifacts
+  fi
+
+  TIMEOUT=${CLI_TIMEOUT} docker-compose -f ${COMPOSE_FILE} up -d 2>&1
   if [ $? -ne 0 ]; then
     echo "ERROR !!!! Unable to start network"
-    sleep 5
     logs
     exit 1
   fi
+
+  #sleep 5
+  createChannel
+  #sleep 5
+  joinChannel ${ORG1}
+  joinChannel ${ORG2}
+  joinChannel ${ORG3}
+  installChaincode ${ORG1} ${CHAINCODE_NAME} ${CHAINCODE_PATH}
+  installChaincode ${ORG2} ${CHAINCODE_NAME} ${CHAINCODE_PATH}
+  installChaincode ${ORG3} ${CHAINCODE_NAME} ${CHAINCODE_PATH}
+  #sleep 5
+  instantiateChaincode ${ORG1} ${CHAINCODE_NAME} ${CHAINCODE_INIT}
   logs
 }
 
+function info() {
+    #figlet $1
+    echo "**************************************************************************************"
+    echo "$1"
+    echo "**************************************************************************************"
+    sleep 2
+}
+
 function logs () {
-    TIMEOUT=$CLI_TIMEOUT COMPOSE_HTTP_TIMEOUT=$CLI_TIMEOUT docker-compose -f $COMPOSE_FILE logs -f
+    TIMEOUT=${CLI_TIMEOUT} COMPOSE_HTTP_TIMEOUT=${CLI_TIMEOUT} docker-compose -f ${COMPOSE_FILE} logs -f
 }
 
 function networkDown () {
   docker-compose -f ${COMPOSE_FILE} down
   # Don't remove containers, images, etc if restarting
   if [ "$MODE" != "restart" ]; then
-    removeDockers
-    removeUnwantedImages
+    clean
   fi
+}
+
+function clean() {
+  removeDockersFromCompose
+  removeDockersWithDomain
+  removeUnwantedImages
 }
 
 # Print the usage message
@@ -146,9 +224,10 @@ if [ "${MODE}" == "up" ]; then
   networkUp
 elif [ "${MODE}" == "down" ]; then
   networkDown
+elif [ "${MODE}" == "clean" ]; then
+  clean
 elif [ "${MODE}" == "generate" ]; then
-  removeDockers
-  removeUnwantedImages
+  clean
   removeArtifacts
   generateArtifacts
 elif [ "${MODE}" == "restart" ]; then
@@ -156,6 +235,14 @@ elif [ "${MODE}" == "restart" ]; then
   networkUp
 elif [ "${MODE}" == "logs" ]; then
   logs
+elif [ "${MODE}" == "channel" ]; then
+  createChannel
+elif [ "${MODE}" == "join" ]; then
+  joinChannel ${ORG1}
+elif [ "${MODE}" == "install" ]; then
+  installChaincode ${ORG1} ${CHAINCODE_NAME} ${CHAINCODE_PATH}
+elif [ "${MODE}" == "instantiate" ]; then
+  instantiateChaincode ${ORG1} ${CHAINCODE_NAME} ${CHAINCODE_INIT}
 else
   printHelp
   exit 1
