2ae1ce24b136cb15fcf5439690122ad9d247d26f
VladimirStarostenkov <vladimir.starostenkov@gmail.com>
2017-08-10 13:52:14 +0300

    Instruction smart contract refactoring

--- chaincode/go/instruction/instruction.go
+++ chaincode/go/instruction/instruction.go
@@ -10,6 +10,7 @@ import (
 	"strings"
 	"time"
 	"io/ioutil"
+	"errors"
 
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 	pb "github.com/hyperledger/fabric/protos/peer"
@@ -19,38 +20,10 @@ var logger = shim.NewLogger("InstructionChaincode")
 
 const indexName = `Instruction`
 
-// InstructionChaincode
-type InstructionChaincode struct {
-}
-
-type InstructionValue struct {
-	DeponentFrom 	string 	`json:"deponentFrom"`
-	DeponentTo		string 	`json:"deponentTo"`
-	Status      	string 	`json:"status"`
-	Initiator 		string 	`json:"initiator"`
-}
-
 const (
 	InitiatorIsTransferer = "transferer"
 	InitiatorIsReceiver   = "receiver"
-)
 
-const (
-	ArgIndexTransfererDeponent 	= 0
-	ArgIndexTransfererAccount 	= 1
-	ArgIndexTransfererDivision 	= 2
-	ArgIndexReceiverDeponent 	= 3
-	ArgIndexReceiverAccount 	= 4
-	ArgIndexReceiverDivision 	= 5
-	ArgIndexSecurity 			= 6
-	ArgIndexQuantity 			= 7
-	ArgIndexReference 			= 8
-	ArgIndexInstructionDate 	= 9
-	ArgIndexTradeDate 			= 10
-	ArgIndexReason 				= 11
-)
-
-const (
 	InstructionInitiated 	= "initiated"
 	InstructionMatched 		= "matched"
 	InstructionExecuted 	= "executed"
@@ -58,17 +31,10 @@ const (
 	InstructionCanceled 	= "canceled"
 )
 
-type Balance struct {
-	Account 		string 	`json:"account"`
-	Division 		string 	`json:"division"`
+type InstructionChaincode struct {
 }
 
-type Reason struct {
-	Document 		string 	`json:"document"`
-	Description 	string 	`json:"description"`
-	DocumentDate 	string 	`json:"documentDate"`
-}
-
+// Instruction is the main data type stored in ledger
 type Instruction struct {
 	Transferer      Balance `json:"transferer"`
 	Receiver        Balance `json:"receiver"`
@@ -84,7 +50,35 @@ type Instruction struct {
 	Reason          Reason 	`json:"reason"`
 }
 
-func (this *Instruction) ToStringArray() ([]string) {
+type Balance struct {
+	Account 		string 	`json:"account"`
+	Division 		string 	`json:"division"`
+}
+
+type Reason struct {
+	Document 		string 	`json:"document"`
+	Description 	string 	`json:"description"`
+	DocumentDate 	string 	`json:"documentDate"`
+}
+
+// required for history
+type KeyModificationValue struct {
+	TxId      string 			`json:"txId"`
+	Value     InstructionValue  `json:"value"`
+	Timestamp string 			`json:"timestamp"`
+	IsDelete  bool   			`json:"isDelete"`
+}
+
+// required for history
+type InstructionValue struct {
+	DeponentFrom 	string 	`json:"deponentFrom"`
+	DeponentTo		string 	`json:"deponentTo"`
+	Status      	string 	`json:"status"`
+	Initiator 		string 	`json:"initiator"`
+}
+
+// **** Instruction Methods **** //
+func (this *Instruction) toStringArray() ([]string) {
 	return []string{
 		this.Transferer.Account,
 		this.Transferer.Division,
@@ -98,17 +92,155 @@ func (this *Instruction) ToStringArray() ([]string) {
 	}
 }
 
-func (this *Instruction) ToCompositeKey(stub shim.ChaincodeStubInterface) (string, error) {
-	return stub.CreateCompositeKey(indexName, this.ToStringArray())
+func (this *Instruction) toCompositeKey(stub shim.ChaincodeStubInterface) (string, error) {
+	return stub.CreateCompositeKey(indexName, this.toStringArray())
 }
 
-type KeyModificationValue struct {
-	TxId      string 			`json:"txId"`
-	Value     InstructionValue  `json:"value"`
-	Timestamp string 			`json:"timestamp"`
-	IsDelete  bool   			`json:"isDelete"`
+func (this *Instruction) existsIn(stub shim.ChaincodeStubInterface) (bool) {
+	compositeKey, err := this.toCompositeKey(stub)
+	if err != nil {
+		return false
+	}
+
+	bytes, _ := stub.GetState(compositeKey)
+	if bytes == nil {
+		return false
+	}
+
+	return true
 }
 
+func (this *Instruction) loadFrom(stub shim.ChaincodeStubInterface) (error) {
+	compositeKey, err := this.toCompositeKey(stub)
+	if err != nil {
+		return err
+	}
+
+	bytes, err := stub.GetState(compositeKey)
+	if err != nil {
+		return err
+	}
+
+	return this.fillFromLedgerValue(bytes)
+}
+
+func (this *Instruction) upsertIn(stub shim.ChaincodeStubInterface) (error) {
+	compositeKey, err := this.toCompositeKey(stub)
+	if err != nil {
+		return err
+	}
+
+	value, err := this.toLedgerValue()
+	if err != nil {
+		return err
+	}
+
+	err = stub.PutState(compositeKey, value)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (this *Instruction) matchIf(stub shim.ChaincodeStubInterface, desiredInitiator string) pb.Response {
+	err := this.loadFrom(stub)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	if this.Initiator != desiredInitiator {
+		return shim.Error("Instruction is already created by " + this.Initiator)
+	}
+
+	this.Status = InstructionMatched
+	err = this.upsertIn(stub)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	return shim.Success([]byte("Instruction was successfully matched."))
+}
+
+func (this *Instruction) initiateIn(stub shim.ChaincodeStubInterface) pb.Response {
+	compositeKey, err := this.toCompositeKey(stub)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	this.Status = InstructionInitiated
+	bytes, err := this.toLedgerValue()
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	err = stub.PutState(compositeKey, bytes)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	return shim.Success([]byte("Instruction was successfully initiated."));
+}
+
+func (this *Instruction) fillFromCompositeKeyParts(compositeKeyParts []string) (error) {
+	if len(compositeKeyParts) != 9 {
+		return errors.New("Composite key parts array length must be 9.")
+	}
+
+	this.Transferer.Account 	= compositeKeyParts[0]
+	this.Transferer.Division 	= compositeKeyParts[1]
+	this.Receiver.Account 		= compositeKeyParts[2]
+	this.Receiver.Division 		= compositeKeyParts[3]
+	this.Security 				= compositeKeyParts[4]
+	this.Quantity 				= compositeKeyParts[5]
+	this.Reference 				= compositeKeyParts[6]
+	this.InstructionDate 		= compositeKeyParts[7]
+	this.TradeDate 				= compositeKeyParts[8]
+
+	return nil
+}
+
+func (this *Instruction) fillFromArgs(args []string) (error) {
+	if len(args) < 12 {
+		return errors.New("Incorrect number of arguments. Expecting >= 12.")
+	}
+
+	this.DeponentFrom			= args[0]
+	this.Transferer.Account 	= args[1]
+	this.Transferer.Division 	= args[2]
+	this.DeponentTo				= args[3]
+	this.Receiver.Account 		= args[4]
+	this.Receiver.Division 		= args[5]
+	this.Security 				= args[6]
+	this.Quantity 				= args[7]
+	this.Reference 				= args[8]
+	this.InstructionDate 		= args[9]
+	this.TradeDate 				= args[10]
+	//this.Reason					= args[11]
+
+	return nil
+}
+
+func (this *Instruction) toLedgerValue() ([]byte, error) {
+	return json.Marshal([]string{this.DeponentFrom, this.DeponentTo, this.Status, string(this.Initiator)})
+}
+
+func (this *Instruction) fillFromLedgerValue(bytes []byte) (error) {
+	var str []string
+	err := json.Unmarshal(bytes, &str)
+	if err != nil {
+		return err
+	}
+
+	this.DeponentFrom = str[0]
+	this.DeponentTo = str[1]
+	this.Status = str[2]
+	this.Initiator = str[3]
+
+	return nil
+}
+
+// **** Chaincode Methods **** //
 func (t *InstructionChaincode) Init(stub shim.ChaincodeStubInterface) pb.Response  {
 	logger.Info("########### InstructionChaincode Init ###########")
 
@@ -165,137 +297,44 @@ func (t *InstructionChaincode) Invoke(stub shim.ChaincodeStubInterface) pb.Respo
 	return shim.Error(err)
 }
 
-func getOrganization(certificate []byte) string {
-	data := certificate[strings.Index(string(certificate), "-----"):strings.LastIndex(string(certificate), "-----")+5]
-	block, _ := pem.Decode([]byte(data))
-	cert, _ := x509.ParseCertificate(block.Bytes)
-	organization := cert.Issuer.Organization[0]
-
-	return organization
-}
-
-func getCreatorOrganization(stub shim.ChaincodeStubInterface) string {
-	certificate, _ := stub.GetCreator()
-	return getOrganization(certificate)
-}
-
-func getMyOrganization() string {
-	// TODO get the filename from $CORE_PEER_TLS_ROOTCERT_FILE
-	// better way perhaps is to pass a flag in transient map to nsd peer to ask to check against book chaincode
-	certFilename := "/etc/hyperledger/fabric/peer.crt"
-	certificate, err := ioutil.ReadFile(certFilename)
-	if err != nil {
-		logger.Debugf("cannot read my peer's certificate file %s", certFilename)
-		return ""
-	}
-
-	return getOrganization(certificate)
-}
-
 func (t *InstructionChaincode) receive(stub shim.ChaincodeStubInterface, args []string) pb.Response {
-	// deponentFrom, accountFrom, divisionFrom, deponentTo, accountTo, divisionTo,
-	// security, quantity, reference, instructionDate, tradeDate, reason
-	if len(args) != 12 {
-		return shim.Error("Incorrect number of arguments. Expecting 12.")
-	}
-
-	instruction := Instruction{
-		Transferer: Balance{
-			Account:		args[ArgIndexTransfererAccount],
-			Division: 		args[ArgIndexTransfererDivision]},
-		Receiver: Balance{
-			Account:		args[ArgIndexReceiverAccount],
-			Division: 		args[ArgIndexReceiverDivision]},
-		Security: 			args[ArgIndexSecurity],
-		Quantity: 			args[ArgIndexQuantity],
-		Reference: 			args[ArgIndexReference],
-		InstructionDate: 	args[ArgIndexInstructionDate],
-		TradeDate: 			args[ArgIndexTradeDate],
-	}
-
-	compositeKey, err := instruction.ToCompositeKey(stub)
+	instruction := Instruction{}
+	err := instruction.fillFromArgs(args)
 	if err != nil {
 		return shim.Error(err.Error())
 	}
 
-	bytes, _ := stub.GetState(compositeKey)
-	if bytes == nil {
-		// Instruction does not exist
-		return t.initiate(stub, instruction, InstructionValue{
-			DeponentFrom: 	args[ArgIndexTransfererDeponent],
-			DeponentTo: 	args[ArgIndexReceiverDeponent],
-			Status:			InstructionInitiated,
-			Initiator:		InitiatorIsReceiver})
+	if instruction.existsIn(stub) {
+		return instruction.matchIf(stub, InitiatorIsTransferer)
 	} else {
-		// Instruction do exist - match it
-		var value InstructionValue
-		err = json.Unmarshal(bytes, &value)
-		if err != nil {
-			return shim.Error(err.Error())
+		instruction.Status = InstructionInitiated
+		if instruction.upsertIn(stub) != nil {
+			return shim.Error("Instruction initialization error.")
 		}
-
-		if value.Initiator == InitiatorIsTransferer {
-			value.Status = InstructionMatched
-			return t.match(stub, instruction, value)
-		} else {
-			return shim.Error("Access denied.")
-		}
+		return shim.Success([]byte("Instruction created."))
 	}
 }
 
 func (t *InstructionChaincode) transfer(stub shim.ChaincodeStubInterface, args []string) pb.Response {
-	// deponentFrom, accountFrom, divisionFrom, deponentTo, accountTo, divisionTo,
-	// security, quantity, reference, instructionDate, tradeDate, reason
-	if len(args) != 12 {
-		return shim.Error("Incorrect number of arguments. Expecting 12.")
-	}
-
-	key := Instruction{
-		Transferer: Balance{
-			Account:		args[ArgIndexTransfererAccount],
-			Division: 		args[ArgIndexTransfererDivision]},
-		Receiver: Balance{
-			Account:		args[ArgIndexReceiverAccount],
-			Division: 		args[ArgIndexReceiverDivision]},
-		Security: 			args[ArgIndexSecurity],
-		Quantity: 			args[ArgIndexQuantity],
-		Reference: 			args[ArgIndexReference],
-		InstructionDate: 	args[ArgIndexInstructionDate],
-		TradeDate: 			args[ArgIndexTradeDate],
-	}
-
-	compositeKey, err := key.ToCompositeKey(stub)
+	instruction := Instruction{}
+	err := instruction.fillFromArgs(args)
 	if err != nil {
 		return shim.Error(err.Error())
 	}
 
-	bytes, _ := stub.GetState(compositeKey)
-	if bytes == nil {
-		// Instruction does not exist
-		return t.initiate(stub, key, InstructionValue{
-			DeponentFrom: 	args[ArgIndexTransfererDeponent],
-			DeponentTo: 	args[ArgIndexReceiverDeponent],
-			Status:			InstructionInitiated,
-			Initiator:		InitiatorIsTransferer})
+	if instruction.existsIn(stub) {
+		return instruction.matchIf(stub, InitiatorIsReceiver)
 	} else {
-		// Instruction do exist - match it
-		var value InstructionValue
-		err = json.Unmarshal(bytes, &value)
-		if err != nil {
-			return shim.Error(err.Error())
+		instruction.Status = InstructionInitiated
+		if instruction.upsertIn(stub) != nil {
+			return shim.Error("Instruction initialization error.")
 		}
-
-		if value.Initiator == InitiatorIsReceiver {
-			value.Status = InstructionMatched
-			return t.match(stub, key, value)
-		} else {
-			return shim.Error("Access denied.")
-		}
+		return shim.Success([]byte("Instruction created."))
 	}
 }
 
 func (t *InstructionChaincode) initiate(stub shim.ChaincodeStubInterface, instruction Instruction, value InstructionValue) pb.Response {
-	compositeKey, err := instruction.ToCompositeKey(stub)
+	compositeKey, err := instruction.toCompositeKey(stub)
 	if err != nil {
 		return shim.Error(err.Error())
 	}
@@ -315,7 +354,7 @@ func (t *InstructionChaincode) initiate(stub shim.ChaincodeStubInterface, instru
 }
 
 func (t *InstructionChaincode) match(stub shim.ChaincodeStubInterface, instruction Instruction, value InstructionValue) pb.Response {
-	compositeKey, err := instruction.ToCompositeKey(stub)
+	compositeKey, err := instruction.toCompositeKey(stub)
 	if err != nil {
 		return shim.Error(err.Error())
 	}
@@ -383,32 +422,20 @@ func (t *InstructionChaincode) query(stub shim.ChaincodeStubInterface, args []st
 			return shim.Error(err.Error())
 		}
 
+		instruction := Instruction{}
+
+		err = instruction.fillFromLedgerValue(response.Value)
+		if err != nil {
+			return shim.Error(err.Error())
+		}
+
 		_, compositeKeyParts, err := stub.SplitCompositeKey(response.Key)
 		if err != nil {
 			return shim.Error(err.Error())
 		}
-
-		var value InstructionValue
-		err = json.Unmarshal(response.Value, &value)
+		err = instruction.fillFromCompositeKeyParts(compositeKeyParts)
 		if err != nil {
 			return shim.Error(err.Error())
-		}
-
-		instruction := Instruction{
-			Transferer: Balance{
-				Account: compositeKeyParts[0],
-				Division: compositeKeyParts[1]},
-			Receiver: Balance{
-				Account: compositeKeyParts[2],
-				Division: compositeKeyParts[3]},
-			Security: compositeKeyParts[4],
-			Quantity: compositeKeyParts[5],
-			Reference: compositeKeyParts[6],
-			InstructionDate: compositeKeyParts[7],
-			TradeDate: compositeKeyParts[8],
-			Status: value.Status,
-			Initiator: value.Initiator,
-			Reason: Reason{Document: "", Description: "", DocumentDate: ""},
 		}
 
 		instructions = append(instructions, instruction)
@@ -429,21 +456,13 @@ func (t *InstructionChaincode) history(stub shim.ChaincodeStubInterface, args []
 			"security, quantity, reference, instructionDate, tradeDate")
 	}
 
-	key := Instruction{
-		Transferer: Balance{
-			Account:		args[ArgIndexTransfererAccount],
-			Division: 		args[ArgIndexTransfererDivision]},
-		Receiver: Balance{
-			Account:		args[ArgIndexReceiverAccount],
-			Division: 		args[ArgIndexReceiverDivision]},
-		Security: 			args[ArgIndexSecurity],
-		Quantity: 			args[ArgIndexQuantity],
-		Reference: 			args[ArgIndexReference],
-		InstructionDate: 	args[ArgIndexInstructionDate],
-		TradeDate: 			args[ArgIndexTradeDate],
+	instruction := Instruction{}
+	err := instruction.fillFromArgs(args)
+	if err != nil {
+		return shim.Error(err.Error())
 	}
 
-	compositeKey, err := key.ToCompositeKey(stub)
+	compositeKey, err := instruction.toCompositeKey(stub)
 	if err != nil {
 		return shim.Error(err.Error())
 	}
@@ -487,6 +506,35 @@ func (t *InstructionChaincode) history(stub shim.ChaincodeStubInterface, args []
 	return shim.Success(result)
 }
 
+// **** Security Methods **** //
+func getOrganization(certificate []byte) string {
+	data := certificate[strings.Index(string(certificate), "-----"):strings.LastIndex(string(certificate), "-----")+5]
+	block, _ := pem.Decode([]byte(data))
+	cert, _ := x509.ParseCertificate(block.Bytes)
+	organization := cert.Issuer.Organization[0]
+
+	return organization
+}
+
+func getCreatorOrganization(stub shim.ChaincodeStubInterface) string {
+	certificate, _ := stub.GetCreator()
+	return getOrganization(certificate)
+}
+
+func getMyOrganization() string {
+	// TODO get the filename from $CORE_PEER_TLS_ROOTCERT_FILE
+	// better way perhaps is to pass a flag in transient map to nsd peer to ask to check against book chaincode
+	certFilename := "/etc/hyperledger/fabric/peer.crt"
+	certificate, err := ioutil.ReadFile(certFilename)
+	if err != nil {
+		logger.Debugf("cannot read my peer's certificate file %s", certFilename)
+		return ""
+	}
+
+	return getOrganization(certificate)
+}
+
+// **** main method **** //
 func main() {
 	err := shim.Start(new(InstructionChaincode))
 	if err != nil {
