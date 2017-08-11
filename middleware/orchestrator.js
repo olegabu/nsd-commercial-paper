cd4f725515852e75e8b4e02f53aba73a60291b80
Maksim Chartkou <maksim.chartkou@altoros.com>
2017-08-11 16:33:43 +0300

    set status

--- middleware/orchestrator.js
+++ middleware/orchestrator.js
@@ -1,14 +1,29 @@
 
 module.exports = function(require) {
+
   let log4js  = require('log4js');
   let logger  = log4js.getLogger('orchestrator');
 
+  const TYPE_ENDORSER_TRANSACTION = 'ENDORSER_TRANSACTION';
+  const INSTRUCTION_CHANNEL = 'instruction';
+  const BOOK_CHANNEL = 'depository';
+
+  // TODO: move somewhere =)
   const ORG = process.env.ORG || null;
+  var config = require('../config.json');
+
+  const USERNAME = config.user.username;
 
   if(ORG != 'nsd'){
     logger.info('Disabled for common members');
     return;
   }
+
+  logger.info('**************    ORCHESTRATOR     ******************');
+  logger.info('Admin     : ' + USERNAME);
+  logger.info('Org name  : ' + ORG);
+
+
 
   let invoke = require('../lib-fabric/invoke-transaction.js');
   let peerListener = require('../lib-fabric/peer-listener.js');
@@ -17,47 +32,81 @@ module.exports = function(require) {
 
   peerListener.registerBlockEvent(function(block) {
     try {
-      let channelId = block.data.data[0].payload.header.channel_header.channel_id;
-      let type = block.data.data[0].payload.header.channel_header.type;
+      let type      = getTransactionType(block);
+      let channelId = getTransactionChannel(block);
+      logger.info('got block no. %d: event %s on channel %s', block.header.number, type, channelId);
 
-      logger.info('got block %d event %s on channel %s: ', block.header.number, type, channelId);
+      if(type === TYPE_ENDORSER_TRANSACTION) {
 
-      if(type === 'ENDORSER_TRANSACTION') {
         //TODO collect from all data and actions elements
-        let extension =
-          block.data.data[0].payload.data.actions[0].payload.action.proposal_response_payload.extension;
+        let extension = block.data.data[0].payload.data.actions[0].payload.action.proposal_response_payload.extension;
 
-        logger.debug('events', extension.events);
+        let event = extension.events;
+        logger.debug('got event: ', event.event_name);
 
-        if(extension.events.event_name === 'Instruction.matched') {
-          let instruction = JSON.parse(extension.events.payload.toString());
+        if(event.event_name === 'Instruction.matched') {
+          let instruction = JSON.parse(event.payload.toString());
           logger.info('Instruction.matched', instruction);
 
           //TODO get peer url from network config
-          invoke.invokeChaincode([ 'peer0.nsd.nsd.ru:7051' ], 'depository', 'book', 'move',
-            [instruction.transferer.account, instruction.transferer.division,
-              instruction.receiver.account, instruction.receiver.division,
-              instruction.security, instruction.quantity],
+          logger.info('Moving balance %s/5s => %s/%s',
+              instruction.transferer.account,
+              instruction.transferer.division,
+              instruction.receiver.account,
+              instruction.receiver.division
+          );
+          invoke.invokeChaincode([ 'peer0.nsd.nsd.ru:7051' ], BOOK_CHANNEL, 'book', 'move',
+            [
+              instruction.transferer.account,
+              instruction.transferer.division,
+              instruction.receiver.account,
+              instruction.receiver.division,
+              instruction.security,
+              instruction.quantity
+            ],
             //TODO user with login 'admin' has to be logged to the web gui, use admin identity that api server operates under
-            'admin', 'nsd')
-          .then(function(transactionId) {
-            logger.debug('invoked', transactionId);
-            //TODO register to wait for this transaction to commit which means the quantity moved from transferer to receiver
-            // then call instruction.status() on channelId to raise the status to 'executed'
-            // on error raise status to 'declined'
+            USERNAME, ORG)
+            .then(function(transactionId) {
+              return 'executed';
+            })
+            .catch(function(e) {
+              return 'declined';
+            })
+            .then(function(status) {
+              logger.debug('balance moved with result:', status);
 
-            //TODO better way is to have Book raise an event that move was successful,
-            // then the orchestrator reacts to it by calling instruction.status().
-            // In the above scenario if the orchestrator dies while waiting for transactionId to commit it'll never get set to executed
+              //TODO better way is to have Book raise an event that move was successful,
+              // then the orchestrator reacts to it by calling instruction.status().
+              // In the above scenario if the orchestrator dies while waiting for transactionId to commit it'll never get set to executed
+
+              // update instruction status
+              logger.info('Updating status for', JSON.strigify(instruction));
+              return invoke.invokeChaincode([ 'peer0.nsd.nsd.ru:7051' ], INSTRUCTION_CHANNEL, 'instruction', 'status',
+                [
+                  instruction.deponentFrom,
+                  instruction.transferer.account,
+                  instruction.transferer.division,
+
+                  instruction.deponentTo,
+                  instruction.receiver.account,
+                  instruction.receiver.division,
+
+                  instruction.security,
+                  instruction.quantity,
+                  instruction.reference,
+                  instruction.instructionDate,
+                  instruction.tradeDate,
+
+                  status
+                ],
+                USERNAME, ORG)
+              .then(function() {
+                logger.debug('status set for instruction', status);
+              })
+
           })
         }
 
-        /*logger.info('response: ', extension.response);
-
-        if(extension.response.payload) {
-          let o = JSON.parse(extension.response.payload.toString());
-          logger.info('responsePayload: ', o);
-        }*/
       }
     } catch(e) {
       logger.warn(e);
@@ -66,3 +115,15 @@ module.exports = function(require) {
   });
 
 };
+
+
+
+
+
+function getTransactionType(block){
+  return block.data.data[0].payload.header.channel_header.type;
+}
+
+function getTransactionChannel(block){
+  return block.data.data[0].payload.header.channel_header.channel_id;
+}
