e822313af3fc114c0593f0d887097ddf1429a547
Maksim Chartkou <maksim.chartkou@altoros.com>
2018-03-04 17:01:17 +0300

    remove unused stuff

--- chaincode/go/src/instruction/instruction_test.go
+++ chaincode/go/src/instruction/instruction_test.go
@@ -1,26 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"testing"
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 	"github.com/olegabu/nsd-commercial-paper-common/assert"
 )
 
 
-func checkState(t *testing.T, stub *shim.MockStub, expectedStatus int32,  args []string) {
-
-	byteArgs := [][]byte{}
-	for _, arg := range args {
-		byteArgs = append(byteArgs, []byte(arg))
-	}
-
-	bytes := stub.MockInvoke("1", byteArgs)
-	if bytes.Status != expectedStatus {
-		fmt.Println("Wrong status. Current value: ", bytes.Status,", Expected value: ", expectedStatus, ".")
-		t.FailNow()
-	}
-}
 
 func getInvokeArgs(command string, args []string) [][]byte {
 	byteArgs := [][]byte{}
@@ -168,39 +154,37 @@ func Test_TransferDVP(t *testing.T) {
 	stub := initInstructionCC(t)
 
 
-	transferArguments := [][]byte{
-		[]byte("dvp"), // type
+	transferArguments := getInvokeArgs("transfer", []string{
+		"dvp", // type
 
-		[]byte("transfer"),
+		"MFONISSUEACC",      // accountFrom
+		"19000000000000000", // divisionFrom
 
-		[]byte("MFONISSUEACC"),      // accountFrom
-		[]byte("19000000000000000"), // divisionFrom
+		"RBIOWNER0ACC",      // accountTo
+		"00000000000000000", // divisionTo
 
-		[]byte("RBIOWNER0ACC"),      // accountTo
-		[]byte("00000000000000000"), // divisionTo
+		"RU000ABC0001",      // security
+		"123",         		 // quantity
+		"ref-123",           // reference
+		"2017-12-31",        // instructionDate
+		"2017-12-31",        // tradeDate
 
-		[]byte("RU000ABC0001"),      // security
-		[]byte("123"),				 // quantity
-		[]byte("ref-123"),           // reference
-		[]byte("2017-12-31"),        // instructionDate
-		[]byte("2017-12-31"),        // tradeDate
-
-		[]byte("DE000DB7HWY7"), // deponentFrom
-		[]byte("CA9861913023"), // deponentTo
-		[]byte("memberInstructionId"), // memberInstructionId
-		[]byte("{\"json_reason\":\"any json\"}"), // reason
+		"DE000DB7HWY7", 	 // deponentFrom
+		"CA9861913023", 	 // deponentTo
+		"memberInstructionId", // memberInstructionId
+		"{\"json_reason\":\"any json\"}", // reason
 
 
-		[]byte("40701810000000001000"), // transfererAccount
-		[]byte("f044525505op"), 		// transfererBic
 
-		[]byte("40701810000000001000"), // receiverAccount
-		[]byte("f044525505op"), 		// receiverBic
+		"40701810000000001000", // transfererAccount
+		"f044525505op",     	// transfererBic
 
-		[]byte("30000000"), 			// paymentAmount
-		[]byte("RUB"), 					// paymentCurrency
+		"40701810000000001000", // receiverAccount
+		"f044525505op",     	// receiverBic
 
-	}
+		"30000000",       		// paymentAmount
+		"RUB",          		// paymentCurrency
+	})
 
 
 	res := stub.MockInvoke("1", transferArguments)
