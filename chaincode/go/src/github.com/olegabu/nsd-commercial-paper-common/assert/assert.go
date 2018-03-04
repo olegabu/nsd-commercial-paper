1d417772f1127f372ac1134e3848beed3b5555de
Maksim Chartkou <maksim.chartkou@altoros.com>
2018-03-04 11:20:04 +0300

    assert: apply message optionally

--- chaincode/go/src/github.com/olegabu/nsd-commercial-paper-common/assert/assert.go
+++ chaincode/go/src/github.com/olegabu/nsd-commercial-paper-common/assert/assert.go
@@ -4,21 +4,42 @@ import (
 	"testing"
 	"fmt"
 	pb "github.com/hyperledger/fabric/protos/peer"
+	"strconv"
 )
 
-func Equal(t *testing.T, actual string, expected string, message string ) {
+
+func Equal(t *testing.T, actual string, expected string, message ...string ) {
 	if actual != expected {
-		fmt.Println("Assert error:", message, "Current value: ", actual," , Expected value: ", expected)
+
+		errorMessage := "Equal fail."
+		if len(message) > 0 {
+			errorMessage = message[0]
+		}
+
+		fmt.Println("Assert error:", errorMessage, "Current value: ", actual," , Expected value: ", expected)
 		t.FailNow()
 	}
 }
 
 
-func Ok(t *testing.T, invokeResult pb.Response, message string ) {
+func Ok(t *testing.T, invokeResult pb.Response, message ...string) {
 	if invokeResult.Status != 200 {
-		fmt.Println("Assert error:", message, "Operation failed with status: ", invokeResult.Status, invokeResult.Message)
+
+		errorMessage := "Ok fail."
+		if len(message) > 0 {
+			errorMessage = message[0]
+		}
+
+		fmt.Println("Assert error:", errorMessage, "Operation failed with status: ", invokeResult.Status, invokeResult.Message)
 		t.FailNow()
 	}
 }
 
+
+func IsNumber(message string ) bool {
+	if _, err := strconv.Atoi(message); err != nil {
+		return false
+	}
+	return true
+}
 
