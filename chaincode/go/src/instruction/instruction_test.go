2422c589d24cae4834b86789fc4e4db42f1249f8
Maksim Chartkou <maksim.chartkou@altoros.com>
2018-03-04 15:18:41 +0300

    point out a problem

--- chaincode/go/src/instruction/instruction_test.go
+++ chaincode/go/src/instruction/instruction_test.go
@@ -22,6 +22,15 @@ func checkState(t *testing.T, stub *shim.MockStub, expectedStatus int32,  args [
 	}
 }
 
+func getInvokeArgs(command string, args []string) [][]byte {
+	byteArgs := [][]byte{}
+	byteArgs = append(byteArgs, []byte(command))
+	for _, arg := range args {
+		byteArgs = append(byteArgs, []byte(arg))
+	}
+	return byteArgs
+}
+
 
 func initInstructionCC(t *testing.T) *shim.MockStub {
 
@@ -43,7 +52,7 @@ func initInstructionCC(t *testing.T) *shim.MockStub {
 
 		[]byte("[" + org1 + "," + org2 + "]"),
 	}
-	res := stub.MockInit("1", initArgs)
+	res := stub.MockInit("123", initArgs)
 
 	assert.Ok(t, res, "Init failed");
 	return stub
@@ -68,35 +77,89 @@ func Test_InstructionInit(t *testing.T) {
 
 
 
-func Test_TransferFOP(t *testing.T) {
+func Test_TransferFOP_legacy(t *testing.T) {
 	//stub := shim.NewMockStub("instruction", new(InstructionChaincode))
 	stub := initInstructionCC(t)
 
 
-	transferArguments := [][]byte{
-		[]byte("transfer"),
+	transferArguments := getInvokeArgs("transfer", []string{
+		//"fop", No type here!
+		"MFONISSUEACC",      // accountFrom
+		"19000000000000000", // divisionFrom
 
-		[]byte("MFONISSUEACC"),      // accountFrom
-		[]byte("19000000000000000"), // divisionFrom
+		"RBIOWNER0ACC",      // accountTo
+		"00000000000000000", // divisionTo
 
-		[]byte("RBIOWNER0ACC"),      // accountTo
-		[]byte("00000000000000000"), // divisionTo
+		"RU000ABC0001",      // security
+		"123",        		 // quantity
+		"ref-123",           // reference
+		"2017-12-31",        // instructionDate
+		"2017-12-31",        // tradeDate
 
-		[]byte("RU000ABC0001"),      // security
-		[]byte("123"),				 // quantity
-		[]byte("ref-123"),           // reference
-		[]byte("2017-12-31"),        // instructionDate
-		[]byte("2017-12-31"),        // tradeDate
-
-		[]byte("DE000DB7HWY7"), // deponentFrom
-		[]byte("CA9861913023"), // deponentTo
-		[]byte("memberInstructionId"), // memberInstructionId
-		[]byte("{\"json_reason\":\"any json\"}"), // reason
-	}
+		"DE000DB7HWY7", 	// deponentFrom
+		"CA9861913023", 	// deponentTo
+		"memberInstructionId", // memberInstructionId
+		"{\"json_reason\":\"any json\"}", // reason
+	})
 
 
 	res := stub.MockInvoke("1", transferArguments)
 	assert.Ok(t, res, "Transfer failed");
+
+}
+
+
+
+func Test_TransferFOP(t *testing.T) {
+	//stub := shim.NewMockStub("instruction", new(InstructionChaincode))
+	stub := initInstructionCC(t)
+
+	transferArguments := getInvokeArgs("transfer", []string{
+		"fop",
+		"MFONISSUEACC",      // accountFrom
+		"19000000000000000", // divisionFrom
+
+		"RBIOWNER0ACC",      // accountTo
+		"00000000000000000", // divisionTo
+
+		"RU000ABC0001",      // security
+		"123",        		 // quantity
+		"ref-123",           // reference
+		"2017-12-31",        // instructionDate
+		"2017-12-31",        // tradeDate
+
+		"DE000DB7HWY7", 	// deponentFrom
+		"CA9861913023", 	// deponentTo
+		"memberInstructionId", // memberInstructionId
+		"{\"json_reason\":\"any json\"}", // reason
+	})
+
+	res := stub.MockInvoke("1", transferArguments)
+	assert.Ok(t, res, "Transfer failed");
+
+	//
+	//
+
+	transfer2 := getInvokeArgs("transfer", []string{
+		"fop",
+		"MZ130605006C",
+		"19000000000000000",
+		"MS980129006C",
+		"00000000000000000",
+		"RU000A0JWGG3",
+		"1",
+		"test1",
+		"2018-03-04",
+		"2018-03-04",
+		"CA9861913023",
+		"DE000DB7HWY7",
+		"123",
+		"{}",
+	})
+
+	res2 := stub.MockInvoke("2", transfer2)
+	assert.Ok(t, res2, "Transfer2 failed");
+
 }
 
 
