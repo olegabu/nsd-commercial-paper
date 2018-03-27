acc1d6d52682bca211124ad88513006bd5e01eba
LeonidLe <leonid.leshchenya@altoros.com>
2018-03-27 21:39:04 +0300

    base-intercept.yaml; ledger volume

--- install-cc.sh
+++ install-cc.sh
@@ -1,5 +1,9 @@
 cc_version="1.0"
+sleep 3
 network.sh -m install-chaincode -o $THIS_ORG -v ${cc_version} -n security
+sleep 1
 network.sh -m install-chaincode -o $THIS_ORG -v ${cc_version} -n book
+sleep 1
 network.sh -m install-chaincode -o $THIS_ORG -v ${cc_version} -n instruction
+sleep 1
 network.sh -m install-chaincode -o $THIS_ORG -v ${cc_version} -n position
