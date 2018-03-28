a8b3b3bd6eb4da6c40eee1a4bc6733209209dca1
LeonidLe <leonid.leshchenya@altoros.com>
2018-03-28 16:58:07 +0300

    automatic prerequiistes setup

--- prerequisites-deployment.sh
+++ prerequisites-deployment.sh
@@ -12,19 +12,47 @@ if [ "$forceRemoveRepos" == "-f" ]; then
   rm -rf nsd-commercial-paper-client
 fi
 
+echo "--------------------------------------------"
+echo "----  Clone fabric-starter"
 git clone --depth=1 --branch 2018_03-MAIN_ORG_DEPLOYMENT https://github.com/olegabu/fabric-starter
+
+echo "--------------------------------------------"
+echo "----  Clone nsd-commercial-paper-client"
 git clone --depth=1 --branch develop https://github.com/olegabu/nsd-commercial-paper-client
+
+echo "--------------------------------------------"
+echo "----  update nsd-commercial-paper-client"
+cd nsd-commercial-paper-client
+git pull
+
+echo "--------------------------------------------"
+echo "----  update fabric-starter"
+cd ../fabric-starter
+git pull
 
 
 ###############################
 # (re)init docker
 ###############################
-export FABRIC_DOCKER_VERSION=docker-ce-18.03.0
-[ `command -v apt` ] && echo "Using: init-docker.sh" || echo "Using: init-docker-centos.sh"
-[ `command -v apt` ] && ../fabric-starter/init-docker.sh || ../fabric-starter/init-docker-centos.sh
+echo "--------------------------------------------"
+echo "Current directory: $PWD"
+echo "--------------------------------------------"
+
+if [ `command -v apt` ]; then
+    echo "Using: init-docker.sh"
+    export FABRIC_DOCKER_VERSION=docker-ce=18.03.0~ce-0~ubuntu
+    init-docker.sh
+else
+    echo "Using: init-docker-centos.sh"
+    export FABRIC_DOCKER_VERSION=docker-ce-18.03.0.ce
+    init-docker-centos.sh
+fi
+
 
 ###############################
 # (re)init fabric
 ###############################
 export FABRIC_VERSION=1.1.0
-../fabric-starter/init-fabric.sh
+echo "--------------------------------------------"
+echo "Init fabric: $FABRIC_VERSION"
+init-fabric.sh
